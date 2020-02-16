4b455bf0bb78186919359504e88cbb5d987f4198
Carlos <cpinelly@gmail.com>
2020-02-15 23:32:49 -0500

    Added model Quote. Added fixture for inital data.

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Author
+from .models import Author, Quote
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
 
-    list_display = ('first_name', 'last_name', 'short_description')
+    list_display = ('id', 'first_name', 'last_name', 'short_description')
 
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+
+    list_display = ('body', 'author_id')
 
 
--- api/models.py
+++ api/models.py
@@ -7,5 +7,15 @@ class Author(models.Model):
     last_name = models.CharField('Last Name', max_length=50, blank=True)
     short_description = models.CharField('Short Description', max_length=100, blank=True)
     picture_url = models.CharField('Picture URL', max_length=300, blank=True)
+    lang = models.CharField('Language', max_length=2, choices=(('en', 'English'), ('es', 'Spanish')))
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Quote(models.Model):
+    author_id = models.ForeignKey(Author, null=False, on_delete=models.CASCADE)
+    body = models.TextField('Body', blank=False)
+    is_dayquote = models.BooleanField(default=False)
+    lang = models.CharField('Language', max_length=2, choices=(('en', 'English'), ('es', 'Spanish')))
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 
