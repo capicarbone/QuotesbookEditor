ebde9e23c23e5766c76a9c114fe4a8424f08d8ae
Carlos Pinelly <cpinelly@gmail.com>
2020-02-19 22:24:00 -0500

    Fix on migrations.

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
+    id = models.CharField(max_length=50, primary_key=True, null=False)
     first_name = models.CharField('First Name', max_length=50, blank=False, null=False)
     last_name = models.CharField('Last Name', max_length=50, blank=True)
     short_description = models.CharField('Short Description', max_length=100, blank=True)
