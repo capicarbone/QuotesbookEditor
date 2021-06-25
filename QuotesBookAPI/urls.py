fbe5aa8033cce2713cdb473df68aebe7923cb25d
capi <cpinelly@gmail.com>
2021-06-25 14:25:58 -0500

    Redirect to landing from root.

--- QuotesBookAPI/urls.py
+++ QuotesBookAPI/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='https://quotesbook.flycricket.io/') ),
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
 ]
